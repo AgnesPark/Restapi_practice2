0d6de9a331e2977ebda8e5c4d5fda467c7b0344c
kyuun <96134745+plily93@users.noreply.github.com>
2022-06-25 23:58:25 +0900

    [4end] Create a Python API Client

---
+++ py_client/basic.py
@@ -0,0 +1,15 @@
+from typing import Dict
+import requests
+
+# endpoint = "https://httpbin.org/status/200"
+endpoint = "https://httpbin.org/anything"
+
+get_response = requests.get(endpoint, json={"query":"Hello world"}) # HTTTP Request
+print(get_response.text) # print raw text response
+
+
+# HTTP Request -> HTML
+# REST API HTTP Request -> JSON
+# JavaScript Object Notation ~ Python Dict
+print(get_response.json())
+print(get_response.status_code)
